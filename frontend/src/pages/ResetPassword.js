00d1effa9a033232bd3a79bdc834ea694c2c8235
Shah Jahan <shahjahan.me.official@gmail.com>
2026-02-19 02:32:21 -0800

    Update ResetPassword.js

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import API from "../utils/api";
 import { useLocation, useNavigate } from "react-router-dom";
 
